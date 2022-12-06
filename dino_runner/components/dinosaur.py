82f4f3b81513a4168a03f0583a8f35f3a9ace057
JuanitoSuperEstrella <lucas.merida@fundacion-jala.org>
2022-12-06 19:30:30 -0400

    clase 2: Dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,44 @@
 import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import RUNNING, DUCKING
 
+class Dinosaur(Sprite):
+    POS_X = 80
+    POS_y = 310
+    POS_Y_DUCKING = 345
 
-class Dinosaur:
     def __init__(self):
-        pass
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_y
+        self.step_index = 0
 
-    def update(self):
-        pass
+    def update(self, user_input):
+        if user_input[pygame.K_DOWN]:
+            self.duck()
+        else:
+            self.run()
 
-    def draw(self):
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, self.dino_rect)
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_y
+        self.step_index += 1
+        
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_Y_DUCKING
+        self.step_index += 1
+
+    def jump(self):
         pass
